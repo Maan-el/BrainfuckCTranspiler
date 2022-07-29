f68c8e97d83e1ef96c9bc34a729f8ff24e765c12
Maan-el <manel@pop-os.localdomain>
2022-07-29 20:47:00 -0300

    Forgot some stuff

--- Brainfuck.c
+++ Brainfuck.c
@@ -31,13 +31,19 @@ int ctrPosition(FILE *input) {
 
   while (!feof(input)) {
     char ch = fgetc(input);
-    if (ch == '>')
+
+    switch (ch) {
+    case '>':
       ctr++;
-    else if (ch == '<')
+      break;
+    case '<':
       ctr--;
-    if (ctr > max)
+      break;
+    }
+
+    if (ctr >= max)
       max = ctr;
-    else if (ctr < min)
+    else
       min = ctr;
   }
 
